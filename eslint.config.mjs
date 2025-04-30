b3f1e22e73d5cb047eb4045390e500fe493937d7
Stijn Aa <stijn.aa@capgemini.com>
2025-04-30 16:28:21 +0200

    adde eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,63 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier"],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
+        rules: {
+          "prettier/prettier": ["error"],
+          "react/function-component-definition": [
+            2,
+            {
+              namedComponents: ["arrow-function", "function-declaration"],
+            },
+          ],
+          "react/no-multi-comp": ["error", { "ignoreStateless": true }],
+          "react/jsx-props-no-spreading": "off",
+          "react/prop-types": "off",
+          "react/react-in-jsx-scope": 0,
+          "react/require-default-props": "off",
+          "no-console": ["error"],
+          "@typescript-eslint/no-unused-vars": ["error"],
+          quotes: ["error", "single", { avoidEscape: true }],
+          eqeqeq: ["error", "always"],
+          "import/prefer-default-export": [
+            "error",
+            {
+                "target": "single"
+            }
+          ],
+          "prefer-const": ["error", { destructuring: "any", ignoreReadBeforeAssign: false }],
+          "no-else-return": ["error"],
+          "import/no-duplicates": ["error", { "prefer-inline": false }],
+          "no-empty-pattern": ["error", { allowObjectPatternsAsParameters: true }],
+          "no-multiple-empty-lines": [
+            "error",
+            {
+              max: 1,
+              maxEOF: 0,
+              maxBOF: 0,
+            },
+          ],
+          "@typescript-eslint/consistent-type-imports": "error",
+          "default-case": ["error"],
+          "linebreak-style": ["error", "unix"],
+          "react/self-closing-comp": ["error"],
+          "react/jsx-curly-brace-presence": [
+            "error",
+            {
+              props: "never",
+              children: "never",
+              propElementValues: "always",
+            },
+          ],
+        },
+      },
+    ],
+  }),
 ];
 
 export default eslintConfig;
